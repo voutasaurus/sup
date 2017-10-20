916a8d58dfc9e52fc5ed6340b381a49611583aed
voutasaurus <voutasaurus@gmail.com>
2017-10-19 21:35:31 -0700

    with spinner

--- sup.go
+++ sup.go
@@ -5,17 +5,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
+
+	"github.com/gernest/wow"
+	"github.com/gernest/wow/spin"
 )
 
 func main() {
 	flag.Parse()
 	for {
+		w := wow.New(os.Stdout, spin.Get(spin.Dots), "Requesting")
+		w.Start()
 		start := time.Now()
 		_, err := http.Get("https://" + flag.Arg(0))
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(time.Since(start))
+		w.PersistWith(spin.Spinner{Frames: []string{"👍  "}}, fmt.Sprint(time.Since(start)))
 	}
 }
